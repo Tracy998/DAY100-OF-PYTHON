7e4248c18965131f25e820e8a42212033a4307e6
tracykorir24@gmail.com <tracy98>
2025-02-12 04:42:01 -0500

    initial commit

---
+++ day one assignment.py
@@ -0,0 +1,28 @@
+#addition of two numbers
+num1=15
+num2=35
+total_sum=num1+num2
+print(total_sum)
+
+
+#booleans
+x=500
+y=100
+if (x>y):
+   print ("x is greater than y")
+else:
+   print("x is not greater then y") 
+
+
+#convertung data types
+a=int(55)
+b=str(a)
+print(b)
+
+#list of favourite foods
+a=("mango")
+b=("banana")
+c=("cherry")
+d=("apple")
+e=("pineapple")
+print(str(a))
